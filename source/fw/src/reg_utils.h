14190172b07d7121cdc99663103f936b56682f55
Diego Rosales <diegorosales.25@gmail.com>
2018-11-24 09:17:11 -0600

    Added structs for the Control registers

--- source/fw/src/reg_utils.h
+++ source/fw/src/reg_utils.h
@@ -19,6 +19,73 @@
 #define MISC_DATA_1_REG_ADDR       0x05
 #define MISC_DATA_2_REG_ADDR       0x06
 
+#define CONTROL_REGISTER_ACCESS ((volatile CONTROLLER_REGISTERS_t *)(REGISTERS_BAR))
+
+/////////////////////////////////
+// CODEC I2C Control Register
+/////////////////////////////////
+typedef union {
+    // Individual Fields
+    struct {
+        uint8_t  codec_i2c_data_wr_reg : 1 ; // Bit 0
+        uint8_t  codec_i2c_data_rd_reg : 1 ; // Bit 1
+        uint8_t  controller_busy_reg   : 1 ; // Bit 2
+        uint8_t  codec_init_done_reg   : 1 ; // Bit 3
+        uint8_t  data_in_valid_reg     : 1 ; // Bit 4
+        uint8_t  missed_ack_reg        : 1 ; // Bit 5
+        uint32_t RSVD                  : 25; // Bits 6-30
+        uint8_t  controller_reset_reg  : 1 ; // Bit 31
+    } field;
+    // Complete Value
+    uint32_t value;
+} CODEC_I2C_CTRL_REG_t;
+
+/////////////////////////////////
+// CODEC I2C Address Register
+/////////////////////////////////
+typedef union {
+    // Individual Fields
+    struct {
+        uint32_t codec_i2c_addr_reg;
+    } field;
+    // Complete Value
+    uint32_t value;
+} CODEC_I2C_ADDR_REG_t;
+
+/////////////////////////////////
+// CODEC I2C Data Write Register
+/////////////////////////////////
+typedef union {
+    // Individual Fields
+    struct {
+        uint32_t codec_i2c_wr_data_reg;
+    } field;
+    // Complete Value
+    uint32_t value;
+} CODEC_I2C_WR_DATA_REG_t;
+
+/////////////////////////////////
+// CODEC I2C Data Read Register
+/////////////////////////////////
+typedef union {
+    // Individual Fields
+    struct {
+        uint32_t codec_i2c_rd_data_reg;
+    } field;
+    // Complete Value
+    uint32_t value;
+} CODEC_I2C_RD_DATA_REG_t;
+
+////////////////////////////////////////////////////////
+// Compilation of all controller registers
+////////////////////////////////////////////////////////
+typedef struct {
+    CODEC_I2C_CTRL_REG_t    CODEC_I2C_CTRL_REG;     // Address 0
+    CODEC_I2C_ADDR_REG_t    CODEC_I2C_ADDR_REG;     // Address 1
+    CODEC_I2C_WR_DATA_REG_t CODEC_I2C_WR_DATA_REG;  // Address 2
+    CODEC_I2C_RD_DATA_REG_t CODEC_I2C_RD_DATA_REG;  // Address 3
+} CONTROLLER_REGISTERS_t;
+
 // Control Register Bits
 #define WR_DATA_BIT          1
 #define RD_DATA_BIT          2
