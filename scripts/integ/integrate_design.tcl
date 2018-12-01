ceb66a8acee5ad3d6eba714de24354301f7c1b56
Diego Rosales <diegorosales.25@gmail.com>
2018-11-30 19:16:11 -0600

    Instantiated the DMA mechanism

--- scripts/integ/integrate_design.tcl
+++ scripts/integ/integrate_design.tcl
@@ -11,9 +11,12 @@
 ###################################################
 #### Add the Zynq Processing Unit ####
 # Enable F2P (Fabric to Processing Unit) Interrupts
+# Enable the High Performance AXI Streaming port for DMA (HP0)
 set zynq_configuration [list \
-                            CONFIG.PCW_USE_FABRIC_INTERRUPT {1} \
-                            CONFIG.PCW_IRQ_F2P_INTR         {1} \
+                            CONFIG.PCW_USE_FABRIC_INTERRUPT {1}  \
+                            CONFIG.PCW_IRQ_F2P_INTR         {1}  \
+                            CONFIG.PCW_USE_S_AXI_HP0        {1}  \
+                            CONFIG.PCW_S_AXI_HP0_DATA_WIDTH {64} \
                         ]
 create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
 set_property -dict ${zynq_configuration} [get_bd_cells processing_system7_0]
@@ -37,6 +40,22 @@ set gpio_configuration [list \
                         ]
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_0
 set_property -dict ${gpio_configuration} [get_bd_cells axi_gpio_0]
+
+#### Add the AXI DMA IP
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_dma:7.1 axi_dma_0
+
+set dma_configuration [list \
+                            CONFIG.c_include_sg               {0}  \
+                            CONFIG.c_sg_include_stscntrl_strm {0}  \
+                            CONFIG.c_include_mm2s             {1}  \
+                            CONFIG.c_m_axi_mm2s_data_width    {64} \
+                            CONFIG.c_m_axis_mm2s_tdata_width  {64} \
+                            CONFIG.c_mm2s_burst_size          {8}  \
+                            CONFIG.c_include_s2mm             {0}  \
+                            CONFIG.c_addr_width               {32} \
+                        ] 
+
+set_property -dict ${dma_configuration} [get_bd_cells axi_dma_0]
 
 #### Add the custom IP and make the connections ####
 set packaged_ip_inst_name ${packaged_ip_name}_inst
@@ -111,3 +130,12 @@ apply_bd_automation -rule xilinx.com:bd_rule:axi4               -config {Master 
 connect_bd_intf_net [get_bd_intf_pins axi_gpio_0/GPIO        ]  [get_bd_intf_ports SW  ] 
 connect_bd_intf_net [get_bd_intf_pins axi_gpio_0/GPIO2       ]  [get_bd_intf_ports BTN ]
 connect_bd_net      [get_bd_pins      axi_gpio_0/ip2intc_irpt]  [get_bd_pins  processing_system7_0/IRQ_F2P]
+
+## AXI DMA
+# AXI Stream to the Audio Controller
+connect_bd_intf_net [get_bd_intf_pins axi_dma_0/M_AXIS_MM2S] [get_bd_intf_pins ${packaged_ip_inst_name}/s_axis]
+apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config {Clk "/processing_system7_0/FCLK_CLK0 (100 MHz)" }  [get_bd_pins ${packaged_ip_inst_name}/s_axis_aclk]
+# AXI DMA to the Zynq Processor
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/axi_dma_0/M_AXI_MM2S" intc_ip "Auto" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
+# AXI-Lite DMA to the Zynq Processor
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" intc_ip "/ps7_0_axi_periph" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins axi_dma_0/S_AXI_LITE]
