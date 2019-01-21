b740eeb6bff5401a4f2532904a4caa999a9237c2
Diego Rosales <diegorosales.25@gmail.com>
2019-01-20 18:51:12 -0600

    Added the main RTOS program file

---
+++ source/fw/src/main_rtos_program.c
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////
+// This is the main file that will be called //
+// at the start                              //
+///////////////////////////////////////////////
+
+#include "main_rtos_program.h"
+
+
+
+
+
+
+void main_rtos_program() {
+
+    xil_printf("%c[2J",27);
+    xil_printf("\n\r");
+    xil_printf("======================\n\r");
+    xil_printf("Zybo Sampler V0.01\n\r");
+    xil_printf("======================\n\r");
+
+    while(1);
+}
---
+++ source/fw/src/main_rtos_program.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_RTOS_PROGRAM_H
+#define MAIN_RTOS_PROGRAM_H
+
+// Xilinx Includes
+#include "xil_printf.h"
+#include "xparameters.h" 
+#include "xgpio.h"
+
+// FreeRtos Includes
+#include "FreeRTOS.h"
+#include "task.h"
+
+// Main function
+void main_rtos_program();
+
+
+#endif
