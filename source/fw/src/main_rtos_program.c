903c2d58ff0a9625d60e0af32af6acfcc0729606
Diego Rosales <diegorosales.25@gmail.com>
2019-02-04 14:09:41 -0600

    Updates to the RTOS main

--- source/fw/src/main_rtos_program.c
+++ source/fw/src/main_rtos_program.c
@@ -5,18 +5,63 @@
 
 #include "main_rtos_program.h"
 
+// Xilinx Includes
+#include "xil_printf.h"
+#include "xparameters.h" 
+#include "xgpio.h"
+#include "portmacro.h"
 
+// FreeRTOS Includes
+#include "FreeRTOS.h"
+#include "task.h"
+#include "FreeRTOS_CLI.h"
 
+// Zybo UART Driver for FreeRTOS+CLI
+#include "zybo_uart_driver.h"
 
-
+// CLI Applications
+#include "codec_CLI_apps.h"
 
 void main_rtos_program() {
 
-    xil_printf("%c[2J",27);
-    xil_printf("\n\r");
-    xil_printf("======================\n\r");
-    xil_printf("Zybo Sampler V0.01\n\r");
-    xil_printf("======================\n\r");
+    vUARTCommandConsoleStart( mainUART_COMMAND_CONSOLE_STACK_SIZE, mainUART_COMMAND_CONSOLE_TASK_PRIORITY );
+
+    register_codec_cli_commands();
+
+    vTaskStartScheduler();
+
 
     while(1);
+}
+
+
+
+
+/////////////////////////////////
+// System Initialization Task  //
+/////////////////////////////////
+static void sys_init_task(void *p) {
+    XGpio gpio;
+
+    while(1){
+        ////////
+        // GPIO Configuration
+        ////////
+        xil_printf("Initializing the GPIO...\n\r");
+        // Initialize the GPIO IP
+        XGpio_Initialize(&gpio, 0);
+        // Enable the GPIO Interrupts
+	    enable_gpio_interrupts();
+	    XGpio_InterruptClear(&gpio, 0xffffffff);
+	    XGpio_InterruptEnable(&gpio, 0xffffffff);
+	    XGpio_InterruptGlobalEnable(&gpio);
+
+        ////////
+        // DMA Configuration
+        ////////
+        xil_printf("Initializing the DMA Engine...\n\r");
+    }
+
+    // Delete the task according to the FreeRTOS instructions
+    vTaskDelete(NULL);
 }
