312001ac8cd7f780c71a6a61e7e1aa54e58462c2
Diego Rosales <6503118+DiegoRosales@users.noreply.github.com>
2019-04-10 09:26:32 -0500

    Added sample loading step

--- source/fw/src/sampler/sampler_FreeRTOS_tasks.c
+++ source/fw/src/sampler/sampler_FreeRTOS_tasks.c
@@ -19,6 +19,124 @@
 static INSTRUMENT_INFORMATION_t *instrument_information = NULL;
 static uint8_t                   instrument_info_buffer[MAX_INST_FILE_SIZE];
 
+
+void file_to_buffer( FF_FILE *pxFile, uint8_t *buffer, uint32_t buffer_len ) {
+    size_t xByte;
+    int    iChar;
+
+
+    xil_printf("[INFO] - Loading the file into memory\n\r");
+    memset( buffer, 0x00, buffer_len );
+    /* Read the next chunk of data from the file. */
+//    for( xByte = 0; xByte < buffer_len; xByte++ )
+//    {
+//        iChar = ff_fgetc( pxFile ); // Get byte
+//
+//        if( iChar == -1 )
+//        {
+//            /* No more characters to return. */
+//            ff_fclose( pxFile );
+//            pxFile = NULL;
+//            break;
+//        }
+//        else
+//        {
+//            buffer[ xByte ] = ( uint8_t ) iChar;
+//        }
+//    }
+    ff_fread( buffer, buffer_len, 1, pxFile );
+    Xil_DCacheFlushRange( buffer, buffer_len );
+    xil_printf("[INFO] - File succesfully loaded into memory. Loaded %d bytes. Address = 0x%x\n\r", buffer_len, buffer);
+}
+
+// This functions loads a file into memory. You need to provide the full file path
+uint32_t load_file_to_memory( char *file_name, uint8_t *buffer, uint32_t buffer_len ) {
+    FF_FILE *pxFile = NULL;
+    size_t file_size;
+
+    // Step 0 - Check the inputs
+    if ( buffer == NULL ) {
+        xil_printf("[ERROR] - Pointer to the buffer is NULL! Enable do_malloc option to allocate a new pointer.\n\r");
+        return 0;
+    }
+
+    // Step 1 - Open the file
+    xil_printf("[INFO] - Opening the file: \"%s\"\n\r", file_name);
+    pxFile = ff_fopen( file_name, "r" );
+
+    // Throw an error if the file cannot be opened
+    if ( pxFile == NULL ) {
+        xil_printf("[ERROR] - File %s could not be opened!\n\r", file_name);
+        return 0;
+    }
+
+    // Get the size of the file
+    file_size = ff_filelength( pxFile );
+
+    // If the file is too big, give an error
+    if ( file_size > buffer_len ) {
+        xil_printf( "[ERROR] - The File is too large. File = %d bytes | Buffer Size = %d bytes\n\r", file_size, buffer_len );
+        xil_printf( cliNEW_LINE );
+        return 0;
+    }
+
+    xil_printf("[INFO] - File opened succesfully. File Size = %d bytes\n\r", file_size);
+
+    // Step 2 - Load the file into memory
+    file_to_buffer( pxFile, buffer, buffer_len );
+
+    return file_size;
+}
+
+// This functions loads a file into memory. You need to provide the full file path
+// This function can perform a memory allocation in case the buffer is new
+uint32_t load_file_to_memory_malloc( char *file_name, uint8_t *buffer, uint32_t buffer_len ) {
+    FF_FILE *pxFile = NULL;
+    size_t file_size;
+    uint8_t *new_buffer;
+
+    // Step 1 - Open the file
+    xil_printf("[INFO] - Opening the file: \"%s\"\n\r", file_name);
+    pxFile = ff_fopen( file_name, "r" );
+
+    // Throw an error if the file cannot be opened
+    if ( pxFile == NULL ) {
+        xil_printf("[ERROR] - File %s could not be opened!\n\r", file_name);
+        return 0;
+    }
+
+    // Get the size of the file
+    file_size = ff_filelength( pxFile );
+
+    // If the file is too big, give an error
+    if ( file_size > buffer_len ) {
+        xil_printf( "[ERROR] - The File is too large. File = %d bytes | Buffer Size = %d bytes\n\r", file_size, buffer_len );
+        xil_printf( cliNEW_LINE );
+        return 0;
+    }
+
+    xil_printf("[INFO] - File opened succesfully. File Size = %d bytes\n\r", file_size);
+
+    // Perform memory allocation for the buffer
+    xil_printf( "[INFO] - Performing memory allocation for the buffer. Requesting %d bytes\n\r", file_size );
+    new_buffer = pvPortMalloc( file_size );
+    //new_buffer = malloc( file_size );
+    // Check if malloc was succesfull
+    if ( new_buffer == NULL ) {
+        xil_printf( "[ERROR] - Memory allocation failed. Requested %d bytes\n\r", file_size );
+        return 0;
+    } else {
+        xil_printf( "[INFO] - Memory allocation was succesfull. Buffer address =  0x%x\n\r", new_buffer );
+    }
+
+    // Step 2 - Load the file into memory
+    file_to_buffer( pxFile, new_buffer, buffer_len );
+
+    *buffer = new_buffer;
+
+    return file_size;
+}
+
 void create_sampler_tasks ( void ) {
 
     // Initializes the instrument information data structure
@@ -93,11 +211,6 @@ void load_instrument_task( void *pvParameters ) {
     file_path_t       *path;
     uint32_t          return_value = 1;
 
-    size_t file_size;
-    size_t xByte;
-    int    iChar;
-    FF_FILE *pxFile = NULL;
-
     for( ;; )
     {
         get_notification: 
@@ -120,58 +233,13 @@ void load_instrument_task( void *pvParameters ) {
                 xil_printf("Loading instrument \"%s\"\n\r", path->file_path);
 
                 // Step 1 - Open the json file containing the instrument information
-                xil_printf("Step 1 - Open the JSON File\n\r");
-                pxFile = ff_fopen( &path->file_path, "r" );
-
-                // Throw an error if the file cannot be opened
-                if ( pxFile == NULL ) {
-                    xil_printf("[ERROR] - File %s could not be opened!\n\r", path->file_path);
-                    return_value = 1;
-                    xQueueSend(path->return_handle, &return_value, 1000);
-                    goto get_notification;                    
-                }
-
-                // Get the size of the file
-                file_size = ff_filelength( pxFile );
-
-                // If the file is too big, give an error
-                if ( file_size > MAX_INST_FILE_SIZE ) {
-                    xil_printf( "[ERROR] - Instrument information file is too large. File = %d bytes | max supported = %d bytes", file_size, MAX_INST_FILE_SIZE );
-                    xil_printf( cliNEW_LINE );
-                    return_value = 1;
-                    xQueueSend(path->return_handle, &return_value, 1000);
-                    goto get_notification;
-                }
-
-                xil_printf("Step 1 - Done! - File Size = %d\n\r", file_size);
-
-                // Step 2 - Load the JSON file into memory
-                xil_printf("Step 2 - Load the JSON file into memory\n\r");
-                memset( instrument_info_buffer, 0x00, MAX_INST_FILE_SIZE );
-                /* Read the next chunk of data from the file. */
-                for( xByte = 0; xByte < MAX_INST_FILE_SIZE; xByte++ )
-                {
-                    iChar = ff_fgetc( pxFile ); // Get byte
-
-                    if( iChar == -1 )
-                    {
-                        /* No more characters to return. */
-                        ff_fclose( pxFile );
-                        pxFile = NULL;
-                        break;
-                    }
-                    else
-                    {
-                        instrument_info_buffer[ xByte ] = ( uint8_t ) iChar;
-                    }
-                }
-
-                xil_printf("Step 2 - Done!\n\r");
+                xil_printf("Step 1 - Load the JSON File\n\r");
+                load_file_to_memory( &path->file_path, instrument_info_buffer, MAX_INST_FILE_SIZE );
 
                 // Step 3 - Initialize the instrument information
                 xil_printf("Step 3 - Initializing the instrument information\n\r");
                 if ( instrument_information == NULL ){
-                    instrument_information = init_instrument_information(88, 1);
+                    instrument_information = init_instrument_information(MAX_NUM_OF_KEYS, 1);
 
                     // Check if the initialization succeeded
                     if ( instrument_information == NULL ){
@@ -191,6 +259,39 @@ void load_instrument_task( void *pvParameters ) {
                 decode_instrument_information( &instrument_info_buffer, instrument_information);
                 xil_printf("Step 4 - Done!\n\r");
 
+                // Step 5 - Load all the samples into memory
+                xil_printf("Step 5 - Loading samples into memory...\n\r");
+                int key       = 0;
+		        int vel_range = 0;
+                char full_path[MAX_PATH_LEN];
+                uint32_t file_size;
+                for (key = 0; key < MAX_NUM_OF_KEYS; key++) {
+                    for (vel_range = 0; vel_range < 1; vel_range++) {
+                        if ( instrument_information->key_information[key]->key_voice_information[vel_range]->sample_present != 0 ) {
+
+                            // Copy the full path
+                            memset( full_path, 0x00, MAX_PATH_LEN );
+                            strncat( full_path, path->file_dir, strlen( path->file_dir ));
+                            strncat( full_path, "/", 1);
+                            strncat( full_path, \
+                                     instrument_information->key_information[key]->key_voice_information[vel_range]->sample_path, \
+                                     strlen(instrument_information->key_information[key]->key_voice_information[vel_range]->sample_path) );
+
+                            instrument_information->key_information[key]->key_voice_information[vel_range]->sample_buffer = NULL;
+                            xil_printf("[INFO] - [%d][%d]Loading Sample \"%s\"\n\r", key, vel_range, instrument_information->key_information[key]->key_voice_information[vel_range]->sample_path );
+                            file_size = load_file_to_memory_malloc( full_path, \
+                                                                    &instrument_information->key_information[key]->key_voice_information[vel_range]->sample_buffer, \
+                                                                    0x100000 );
+                            
+                            if ( instrument_information->key_information[key]->key_voice_information[vel_range]->sample_buffer == NULL || file_size == 0 ) {
+                                xil_printf("[ERROR] - There was a problem when loading the samples into memory!!\n\r");
+                                return_value = 1;
+                                xQueueSend(path->return_handle, &return_value, 1000);
+                                goto get_notification;                                
+                            }
+                        }
+                    }
+                }
 
                 xQueueSend(path->return_handle, &return_value, 1000);
             }
--- source/fw/src/sampler/sampler_FreeRTOS_tasks.h
+++ source/fw/src/sampler/sampler_FreeRTOS_tasks.h
@@ -11,9 +11,13 @@
 
 typedef struct {
     char file_path[MAX_PATH_LEN];
+    char file_dir[MAX_PATH_LEN];
     xQueueHandle return_handle;
 } file_path_t;
 
+uint32_t load_file_to_memory( char * file_name, uint8_t *buffer, uint32_t buffer_len );
+uint32_t load_file_to_memory_malloc( char *file_name, uint8_t *buffer, uint32_t buffer_len );
+void file_to_buffer( FF_FILE *pxFile, uint8_t *buffer, uint32_t buffer_len );
 void notification_test_task( void *pvParameters );
 void load_instrument_task( void *pvParameters );
 
