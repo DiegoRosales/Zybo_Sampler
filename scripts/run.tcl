4940242213ec2c1bf1907a9e20ce7e8f101deb01
Diego <diegorosales.25@gmail.com>
2020-04-11 10:25:04 +0200

    Fixed xsct run command from Vivado

--- scripts/run.tcl
+++ scripts/run.tcl
@@ -202,11 +202,13 @@ if {$tool == "vivado"} {
         set xsct [file normalize $::env(XILINX_VITIS)/bin/xsct.bat]
 
         ## Make the command
-        set cmd "$xsct [file normalize [info script]] -cfg [file normalize $parsed_args(cfg)] -stages \"BUILD_WS\""
+        set cmd "$xsct [file normalize [info script]] -cfg [file normalize $parsed_args(cfg)] -stages BUILD_WS"
 
         ## Run the command
         puts "Running command \'$cmd\'"
-        exec [split $cmd]
+        catch {exec [split $cmd]} output
+        puts "Output:"
+        puts $output
 
         puts "\n"
         puts "Done!"
