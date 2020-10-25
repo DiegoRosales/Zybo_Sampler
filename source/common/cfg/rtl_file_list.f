5fe1fcbcaf7e5869eadd23d7757caaf25af9be55
Diego Rosales <6503118+DiegoRosales@users.noreply.github.com>
2020-10-25 14:01:50 +0100

    Cleaned include file list

--- source/common/cfg/rtl_file_list.f
+++ source/common/cfg/rtl_file_list.f
@@ -8,22 +8,9 @@ set synthesis_file_list {
     ${core_root}/rtl/axi_slave_controller.sv
 }
 
-## Simulation files
-set uvm_simulation_file_list {
-    ${core_root}/verif/i2s_vip/i2s_vip_env.sv
-}
-
-## Simulation include files
-set uvm_simulation_env_file_list {
-    ${core_root}/verif/i2s_vip/i2s_vip_agent.svh
-    ${core_root}/verif/i2s_vip/i2s_vip_driver.svh
-    ${core_root}/verif/i2s_vip/i2s_vip_sequencer.svh
-    ${core_root}/verif/i2s_vip/i2s_vip_transfer.svh
-    ${core_root}/verif/i2s_vip/i2s_vip_pkg.svh
-    ${core_root}/verif/i2s_vip/i2s_if.svh
-}
-
 ## Simulation include dirs
 set uvm_simulation_env_incdir_list {
     ${core_root}/gen
+    ${core_root}/verif/i2s_vip/
+    ${core_root}/verif/axi4_lite_bfm/
 }
