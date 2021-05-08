fd31d1f169d2faaf8aa67d91e23f9a6a417ddc53
Diego Rosales <6503118+DiegoRosales@users.noreply.github.com>
2021-05-08 19:26:00 +0200

    Cleanup

--- subsystems/sampler_mixer_unit/cfg/rtl_file_list.f
+++
@@ -1,7 +0,0 @@
-####################
-## codec_unit RTL filelist
-####################
-
-set synthesis_rtl_file_list {
-    ${core_root}/rtl/sampler_mixer.sv
-}
--- subsystems/sampler_mixer_unit/cfg/xilinx_ip_tcl_file_list.f
+++
@@ -1,7 +0,0 @@
-########################
-## List of Xilinx IPs
-
-set xilinx_ip_tcl_list {
-    ${core_root}/rtl/xilinx_ip/axis_fifo_32x64_u8_npm.tcl
-    ${core_root}/rtl/xilinx_ip/ila_probes/sampler_mixer_ILA.tcl
-}
