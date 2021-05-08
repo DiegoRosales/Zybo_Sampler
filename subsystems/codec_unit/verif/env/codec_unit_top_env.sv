213b032867efb9103b445a631cc47293804b5d31
Diego Rosales <6503118+DiegoRosales@users.noreply.github.com>
2021-05-08 12:48:18 +0200

    Cleanup

--- subsystems/codec_unit/verif/env/codec_unit_top_env.sv
+++
@@ -1,6 +0,0 @@
-// CODEC Environment
-
-`include "clock_and_reset_bfm_env.sv"
-`include "i2s_bfm_env.sv"
-`include "axi4_lite_bfm_env.sv"
-`include "codec_unit_top_pkg.svh"
--- subsystems/verification_components/clock_and_reset_bfm/clock_and_reset_bfm_env.sv
+++
@@ -1,4 +0,0 @@
-// clock_and_reset Verification IP
-
-`include "clock_and_reset_if.svh"
-`include "clock_and_reset_bfm_pkg.svh"
